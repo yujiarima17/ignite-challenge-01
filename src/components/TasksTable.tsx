0186e0c05c4967b40428ec42eeba081040ca9c0d
Yuji Arima <arimayuji2@gmail.com>
2023-09-23 10:27:13 -0300

    add task bar layout

--- src/components/TasksTable.tsx
+++ src/components/TasksTable.tsx
@@ -1,11 +1,14 @@
 import styles from "../css/TasksTable.module.css";
+import { PlusCircle } from "phosphor-react";
 export function TasksTable() {
   return (
     <>
       <div className={styles.wrapper}>
-        <form action="">
-          <input type="text" />
-          <button>Criar</button>
+        <form action="" className={styles.taskForm}>
+          <input type="text" placeholder="Adicionar uma nova tarefa!" />
+          <button>
+            Criar <PlusCircle size={20} />
+          </button>
         </form>
         <div className="tasks-list"></div>
       </div>
