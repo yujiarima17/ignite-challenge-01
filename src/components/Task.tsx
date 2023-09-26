7a58d316a911fc08805c04ea7ad430b0dcf36401
Yuji Arima <arimayuji2@gmail.com>
2023-09-25 21:15:09 -0300

    css hover on delete

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,27 @@
 import { Trash } from "phosphor-react";
 import styles from "../css/Task.module.css";
 import { CheckBox } from "./CheckBox";
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, MouseEventHandler } from "react";
 
 interface TaskProp {
   text: string;
   isCompleted: boolean;
   changeIsComplete: ChangeEventHandler<HTMLInputElement> | undefined;
+  deleteTask: MouseEventHandler<HTMLButtonElement> | undefined;
 }
-export function Task({ text, changeIsComplete, isCompleted }: TaskProp) {
+export function Task({
+  text,
+  changeIsComplete,
+  deleteTask,
+  isCompleted,
+}: TaskProp) {
   return (
     <div className={styles.task}>
       <CheckBox changeIsComplete={changeIsComplete} />
       <p className={isCompleted ? styles.completed : ""}>{text}</p>
-      <Trash size={20} />
+      <button onClick={deleteTask}>
+        <Trash size={15} />
+      </button>
     </div>
   );
 }
